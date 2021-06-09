a21e10f0c935148fc88e6f4752bdf4d0a99f570b
Szymon Zygadliński <szygadlinski@gmail.com>
2021-06-10 01:43:51 +0200

    Add Link to PostSummary

--- src/components/features/PostSummary/PostSummary.js
+++ src/components/features/PostSummary/PostSummary.js
@@ -6,25 +6,30 @@ import clsx from 'clsx';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
 
+import { Link } from '@material-ui/core';
+
 import styles from './PostSummary.module.scss';
 
-const Component = ({ className, title, image, price, lastUpdate }) => (
+const Component = ({ className, id, title, image, price, lastUpdate }) => (
   <div className={clsx(className, styles.root)}>
-    <div className={styles.postImage}>
-      {image === ''
-        ? <img src='https://cdn4.iconfinder.com/data/icons/documents-letters-and-stationery/400/doc-14-512.png' alt='no-pic' />
-        : <img src={image} alt='post-pic' />
-      }
-    </div>
-    <div className={styles.postInfo}>
-      <h2>{title}</h2>
-      <p>Last update: {lastUpdate}</p>
-      <h2>{price} $</h2>
-    </div>
+    <Link className={styles.postLink} href={`/post/${id}`} >
+      <div className={styles.postImage}>
+        {image === ''
+          ? <img src='https://cdn4.iconfinder.com/data/icons/documents-letters-and-stationery/400/doc-14-512.png' alt='no-pic' />
+          : <img src={image} alt='post-pic' />
+        }
+      </div>
+      <div className={styles.postInfo}>
+        <h2>{title}</h2>
+        <p>Last update: {lastUpdate}</p>
+        <h2>{price} $</h2>
+      </div>
+    </Link>
   </div>
 );
 
 Component.propTypes = {
+  id: PropTypes.string,
   className: PropTypes.string,
   title: PropTypes.string,
   image: PropTypes.string,
