9384f4946d06a8e7d75224ffc58f548ac5fee580
Szymon Zygadliński <szygadlinski@gmail.com>
2021-06-14 01:45:21 +0200

    Add content & styles for EditingPost component

--- src/components/features/EditingPost/EditingPost.js
+++ src/components/features/EditingPost/EditingPost.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
@@ -6,18 +6,160 @@ import clsx from 'clsx';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
 
+import { TextField, FormControl, InputLabel, Select, MenuItem, Button, OutlinedInput, InputAdornment } from '@material-ui/core';
+
 import styles from './EditingPost.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>EditingPost</h2>
-    {children}
-  </div>
-);
+const Component = ({className, title, content, email, price, phone, city}) => {
+
+  const [status, setStatus] = useState('');
+  const handleStatus = event => {
+    setStatus(event.target.value);
+  };
+
+  const [image, setImage] = useState(null);
+  const handleImage = ({ target }) => {
+    setImage(target.files[0]);
+  };
+
+  return (
+    <div className={clsx(className, styles.root)}>
+      <h1>Editing post</h1>
+
+      <form className={styles.form} action='/' method='POST'>
+        <TextField
+          id='post-title'
+          className={styles.formInput}
+          label='Title'
+          value={title}
+          variant='outlined'
+          required
+          inputProps={{
+            minLength: 10,
+            maxLength: 30,
+          }}
+        />
+
+        <TextField
+          id='post-description'
+          className={styles.formInput}
+          label='Description'
+          value={content}
+          variant='outlined'
+          multiline
+          rows='10'
+          required
+          inputProps={{
+            minLength: 20,
+            maxLength: 5000,
+          }}
+        />
+
+        <TextField
+          id='post-email'
+          className={styles.formInput}
+          label='E-mail'
+          value={email}
+          variant='outlined'
+          type='email'
+          required
+          inputProps={{
+            minLength: 6,
+            maxLength: 100,
+          }}
+        />
+
+        <FormControl variant='outlined' className={styles.formInput} required>
+          <InputLabel id='post-status-label'>Status</InputLabel>
+          <Select
+            labelId='post-status-label'
+            id='post-status'
+            value={status}
+            onChange={handleStatus}
+            label='Status'
+          >
+            <MenuItem value={'draft'}>Draft</MenuItem>
+            <MenuItem value={'published'}>Published</MenuItem>
+            <MenuItem value={'closed'}>Closed</MenuItem>
+          </Select>
+        </FormControl>
+
+        <label htmlFor='post-image'>
+          <Button className={styles.formInput + ' ' + styles.formButton} variant='outlined' component='span'>
+            <input
+              accept='image/*'
+              id='post-image'
+              type='file'
+              onChange={handleImage}
+              hidden
+            />
+            {image ? `Uploaded: ${image.name}` : 'Upload image'}
+          </Button>
+        </label>
+
+        <FormControl className={styles.formInput} variant='outlined'>
+          <InputLabel htmlFor='post-price'>Price</InputLabel>
+          <OutlinedInput
+            id='post-price'
+            type='number'
+            value={price}
+            startAdornment={<InputAdornment position='start'>$</InputAdornment>}
+            labelWidth={40}
+            inputProps={{
+              min: 1,
+              max: 999999,
+            }}
+          />
+        </FormControl>
+
+        <TextField
+          id='post-phone'
+          type='tel'
+          className={styles.formInput}
+          label='Phone'
+          value={phone}
+          variant='outlined'
+          inputProps={{
+            minLength: 9,
+            maxLength: 20,
+          }}
+        />
+
+        <TextField
+          id='post-location'
+          className={styles.formInput}
+          label='Location'
+          value={city}
+          variant='outlined'
+          inputProps={{
+            minLength: 3,
+            maxLength: 30,
+          }}
+        />
+
+        <Button
+          className={styles.formInput + ' ' + styles.formSubmit}
+          type='submit'
+          variant='outlined'
+          size='large'
+        >
+          Update!
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  title: PropTypes.string,
+  content: PropTypes.string,
+  email: PropTypes.string,
+  status: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  phone: PropTypes.string,
+  city: PropTypes.string,
 };
 
 // const mapStateToProps = state => ({
