5131ae67935832a127d6d80025eb4f1d8e26cd1c
Szymon Zygadliński <szygadlinski@gmail.com>
2021-06-14 02:45:01 +0200

    Add content & styles to MyPosts subpage

--- src/components/views/MyPosts/MyPosts.js
+++ src/components/views/MyPosts/MyPosts.js
@@ -3,35 +3,71 @@ import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
+import { connect } from 'react-redux';
+import { getAll } from '../../../redux/postsRedux';
+import { getUserStatus, getUserEmail/*, reduxActionCreator */} from '../../../redux/userRedux';
+
+import { Button, Link } from '@material-ui/core';
+
+import { NotFound } from '../NotFound/NotFound';
+import { PostSummary } from '../../features/PostSummary/PostSummary';
 
 import styles from './MyPosts.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className, posts, userStatus, userEmail}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>MyPosts</h2>
-    {children}
+    {userStatus === 'not-logged-in'
+      ? <NotFound />
+      : <>
+        <div className={styles.header}>
+          <h1>My posts</h1>
+          <Button
+            className={styles.button}
+            component={Link}
+            href='/post/add'
+            variant="outlined"
+            color="inherit"
+            size="large"
+          >
+          + Add new post
+          </Button>
+        </div>
+
+        {posts
+          .filter(post => post.email === userEmail)
+          .sort((a, b) => (
+            new Date(b.lastUpdate) - new Date(a.lastUpdate)
+          ))
+          .map(post => (
+            <PostSummary key={post.id} {...post} />
+          ))
+        }
+      </>
+    }
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  posts: PropTypes.array,
+  userStatus: PropTypes.string,
+  userEmail: PropTypes.string,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+  userStatus: getUserStatus(state),
+  userEmail: getUserEmail(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps/*, mapDispatchToProps*/)(Component);
 
 export {
-  Component as MyPosts,
-  // Container as MyPosts,
+  // Component as MyPosts,
+  Container as MyPosts,
   Component as MyPostsComponent,
 };
