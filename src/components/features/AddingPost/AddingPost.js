ea21321dcd11325753d010e97cc497244ce03871
Szymon Zygadliński <szygadlinski@gmail.com>
2021-06-22 03:17:17 +0200

    Fix displaying posts dates

--- src/components/features/AddingPost/AddingPost.js
+++ src/components/features/AddingPost/AddingPost.js
@@ -49,13 +49,13 @@ const Component = ({ className, userEmail, addPost }) => {
   };
 
   const currentDate = () => {
-    const date = new Date();
-    const day = date.getDate();
+    const date = new Date(Date.now());
     const month = String(date.getMonth() + 1).padStart(2, 0);
+    const day = String(date.getDate()).padStart(2, 0);
     const year = date.getFullYear();
     const hour = String(date.getHours()).padStart(2, 0);
     const minute = String(date.getMinutes()).padStart(2, 0);
-    return `${day}.${month}.${year} ${hour}:${minute}`;
+    return `${month}.${day}.${year} ${hour}:${minute}`;
   };
 
   const history = useHistory();
