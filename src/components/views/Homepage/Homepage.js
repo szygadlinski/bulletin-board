e9a85d60dd0ee6dd3293bcf652901b05cc303564
Szymon Zygadliński <szygadlinski@gmail.com>
2021-06-23 02:38:16 +0200

    Use fetchPosts() in Homepage component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -13,41 +13,47 @@ import { PostSummary } from '../../features/PostSummary/PostSummary';
 
 import styles from './Homepage.module.scss';
 
-const Component = ({ className, posts, userStatus }) => (
-  <div className={clsx(className, styles.root)}>
-    <div className={styles.header}>
-      <h1>Latest posts</h1>
+const Component = ({ className, posts, userStatus, fetchPosts }) => {
 
-      {userStatus === 'not-logged-in'
-        ? ''
-        : <Button
-          className={styles.button}
-          component={Link}
-          href='/post/add'
-          variant="outlined"
-          color="inherit"
-          size="large"
-        >
+  fetchPosts();
+
+  return (
+    <div className={clsx(className, styles.root)}>
+      <div className={styles.header}>
+        <h1>Latest posts</h1>
+
+        {userStatus === 'not-logged-in'
+          ? ''
+          : <Button
+            className={styles.button}
+            component={Link}
+            href='/post/add'
+            variant="outlined"
+            color="inherit"
+            size="large"
+          >
           + Add new post
-        </Button>
+          </Button>
+        }
+      </div>
+
+      {posts
+        .sort((a, b) => (
+          new Date(b.lastUpdate) - new Date(a.lastUpdate)
+        ))
+        .map(post => (
+          <PostSummary key={post.id} {...post} />
+        ))
       }
     </div>
-
-    {posts
-      .sort((a, b) => (
-        new Date(b.lastUpdate) - new Date(a.lastUpdate)
-      ))
-      .map(post => (
-        <PostSummary key={post.id} {...post} />
-      ))
-    }
-  </div>
-);
+  );
+};
 
 Component.propTypes = {
   className: PropTypes.string,
   posts: PropTypes.array,
   userStatus: PropTypes.string,
+  fetchPosts: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
